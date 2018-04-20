2bb7b8b87c2d0ba9173315b8bc93d0cf5db657df
Steven Carbuhn <stevencarbuhn@Stevens-MacBook-Air.local>
2018-04-20 00:45:57 -0600

    Updated form styles for portfolios

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -1,9 +1,8 @@
 class PortfoliosController < ApplicationController
   before_action :set_portfolio_item, only: [:edit, :show, :update, :destroy]
   layout 'portfolio'
-  
   access all: [:show, :index, :angular], user: {except: [:destroy, :new, :create, :update, :edit]}, site_admin: :all
-
+  
   def index
     @portfolio_items = Portfolio.all
   end
@@ -32,7 +31,7 @@ class PortfoliosController < ApplicationController
   def edit
   end
 
-    def update
+  def update
     respond_to do |format|
       if @portfolio_item.update(portfolio_params)
         format.html { redirect_to portfolios_path, notice: 'The record successfully updated.' }
@@ -58,14 +57,14 @@ class PortfoliosController < ApplicationController
   private
 
   def portfolio_params
-    params.require(:portfolio).permit(:title, 
-                                      :subtitle, 
-                                      :body, 
-                                      technologies_attributes: [:name])
+    params.require(:portfolio).permit(:title,
+                                      :subtitle,
+                                      :body,
+                                      technologies_attributes: [:name]
+                                     )
   end
 
-   def set_portfolio_item
+  def set_portfolio_item
     @portfolio_item = Portfolio.find(params[:id])
   end
-
 end
--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -1,6 +1,6 @@
 class Portfolio < ApplicationRecord
   has_many :technologies
-  accepts_nested_attributes_for :technologies, 
+  accepts_nested_attributes_for :technologies,
                                 reject_if: lambda { |attrs| attrs['name'].blank? }
 
   include Placeholder
@@ -10,7 +10,7 @@ class Portfolio < ApplicationRecord
     where(subtitle: 'Angular')
   end
 
-  scope :ruby_on_rails_portfolio_items, -> {where(subtitle:'Ruby on Rails')}
+  scope :ruby_on_rails_portfolio_items, -> { where(subtitle: 'Ruby on Rails') }
 
   after_initialize :set_defaults
 
