11cea122ba9f35460f5a31dbf6eed32ce299f271
Steven Carbuhn <stevencarbuhn@Stevens-MacBook-Air.local>
2018-04-29 03:51:17 -0600

    Refactored alerts into help method

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -60,3 +60,16 @@ module ApplicationHelper
   end
 
 end
+
+
+def alerts
+  alert = (flash[:alert] || flash[:error] || flash[:notice])
+
+  if alert
+    alert_generator alert
+  end
+end
+
+def alert_generator msg
+  js add_gritter(msg, title: "Steven Carbuhn's Portfolio", sticky: false)
+end
